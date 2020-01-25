fe1c02659932fec302d8c60a3b2d90ded55493ad
Jmacro <jonmacrohon@gmail.com>
2020-01-25 23:43:48 +1100

    front end jquery logic to manage login

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -7,6 +7,31 @@ $(function () {
         //need to add some logic
     });
 
+    $("#login-button-proceed").on("click", function () {        
+        //need to add some logic
+        event.preventDefault();
+        console.log("clicked");
+        const usernameInput = $("#login-username").val().trim();
+        const passwordInput = $("#login-password").val().trim();
+        
+        const checkUserExist = {
+            username: usernameInput,
+            password: passwordInput,
+        };
+        // Send the PUT request.
+        $.ajax("/api/users/login", {
+            type: "POST",
+            data: checkUserExist
+        }).then(
+            function (data) {
+                console.log("user has logged in: " + data);                
+                // Reload the page to get the updated list
+                // location.reload();
+            }
+        );   
+
+    });
+
     $("#sign-up-button").on("click", function () {
         //need to add some logic
         console.log("clicked");
