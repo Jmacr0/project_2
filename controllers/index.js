67d4620cbe989692b3a2fe8588124608fcafb234
chstrcpprpt <cjhallen@gmail.com>
2020-01-25 14:18:21 +1100

    added sign up route

--- controllers/index.js
+++ controllers/index.js
@@ -29,4 +29,28 @@ router.post("/api/users", function (req, res) {
     })
 })
 
+// POST route for sign-in page
+router.post("/sign-up", (req, res) => {
+    try {
+        db.Users.create({
+            username: req.body.username,
+            password: req.body.password,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,        
+            country: req.body.country
+        })
+        res.redirect("/login");
+    } catch {
+        res.redirect("/sign-up");
+    }
+    console.log("User created");
+});
+
+// POST route for login page
+router.post("/login", (req, res) => {
+
+});
+
 module.exports = router;
+
+router.get(`/login/${username}/${password}
