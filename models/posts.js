8d1230216cc995ae175aaf962fdc945cd9e24e74
Alex Hoang <alexhoang@Alexs-MacBook-Air-2.local>
2020-01-29 20:57:13 +1100

    newthings

--- models/posts.js
+++ models/posts.js
@@ -21,6 +21,7 @@ module.exports = function (sequelize, DataTypes) {
     // A Post can't be created without an Author due to the foreign key constraint
     Posts.belongsTo(models.Users, {
       foreignKey: {
+        name: 'postId',
         allowNull: false
       }
     });
@@ -33,6 +34,7 @@ module.exports = function (sequelize, DataTypes) {
 
     Posts.hasMany(models.Likes, {
       foreignKey: {
+        name: 'postId',
         allowNull: false
       }
     });    
