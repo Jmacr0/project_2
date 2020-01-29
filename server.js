81b5fa94327bd0dc21c97a238da5e8e2be03bc36
Jmacro <jonmacrohon@gmail.com>
2020-01-29 20:11:54 +1100

    fix missing things

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const routes = require("./controllers");
 
 app.use(routes);
 
-db.sequelize.sync({force: true}).then(function() {
+db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
