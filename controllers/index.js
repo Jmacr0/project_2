afbc37dda958bc14e5818a88aab15b3cd01072a5
chstrcpprpt <cjhallen@gmail.com>
2020-01-30 12:17:54 +1100

    fixed some indentation, edited sign-up route

--- controllers/index.js
+++ controllers/index.js
@@ -21,33 +21,87 @@ router.get("/post", function (req, res) {
 router.get("/profile", function (req, res) {
     db.Users.findOne({
         where: {
-          id: req.params.id
+            id: req.params.id
         },
         include:  {
             model: db.Posts,
             include: [db.Comments, db.Likes]
-          }
-      }).then(function(dbAuthor) {
-          console.log(dbAuthor);
+        }
+    }).then(function(dbAuthor) {
+        console.log(dbAuthor);
         res.render("profile", dbAuthor);
-      });
+    });
     
 });
 
 
-router.post("/api/users", function (req, res) {
-    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
-    console.log(hashedPassword);
-    db.Users.create({
-        username: req.body.username,
-        password: hashedPassword,
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        country: req.body.country
-    }).then(function (newUser) {
-        res.json(newUser);
-    })
-})
+// router.post("/api/users", function (req, res) {
+//     const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+//     console.log(hashedPassword);
+//     db.Users.create({
+//         username: req.body.username,
+//         password: hashedPassword,
+//         firstName: req.body.firstName,
+//         lastName: req.body.lastName,
+//         country: req.body.country
+//     }).then(function (newUser) {
+//         res.json(newUser);
+//     })
+// })
+
+router.post("/users/signup", (req, res) => {
+    
+    const {
+        signUpEmail,
+        signUpFirstName,
+        signUpLastName,
+        signUpUsername,
+        signUpPassword,
+        confirmPassword,
+        signUpCountry
+    } = req.body;
+
+    let errors = [];
+
+    // check required fields
+    if(!signUpEmail || !signUpFirstName || !signUpLastName || !signUpUsername || !signUpPassword || !confirmPassword || !signUpCountry) {
+        errors.push({msg: "Please fill in all fields"});
+    }
+
+    // check passwords match
+    if(signUpPassword !== confirmPassword) {
+        errors.push({msg: "Passwords do not match"});
+    }
+
+    // check password length
+    if(signUpPassword.length < 8) {
+        errors.push({msg: "Password must be at least 8 characters"})
+    }
+
+    // if there is an error, re-render the page with the errors displayed
+    // else, redirect to the login page
+    if(errors.length > 0) {
+        res.render("sign-up", {
+            errors
+        })
+    } else {
+        const hashedPassword = bcrypt.hashSync(signUpPassword, 10);
+        db.Users.create({
+            email: signUpEmail,
+            username: signUpUsername,
+            password: hashedPassword,
+            firstName: signUpFirstName,
+            lastName: signUpLastName,
+            country: signUpCountry
+        }).then(function (newUser) {
+            res.json(newUser);
+        })
+        res.redirect("/login");
+    }
+
+    console.log(errors);
+
+});
 
 router.post("/api/users/login", async function (req, res) {
     const checkUserExist = await db.Users.findOne({
@@ -58,7 +112,7 @@ router.post("/api/users/login", async function (req, res) {
     if (bcrypt.compareSync(req.body.password, checkUserExist.password)){
         console.log(`${checkUserExist.username} is now logged in !`)
     }
-   
+
 })
 
 module.exports = router;
