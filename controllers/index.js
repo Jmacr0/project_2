46877b6bdc46f81e06a4676140aae6d906cfc8ad
chstrcpprpt <cjhallen@gmail.com>
2020-01-30 13:02:59 +1100

    check if email and username are in use - issue with email

--- controllers/index.js
+++ controllers/index.js
@@ -63,7 +63,7 @@ router.post("/users/signup", (req, res) => {
 
     let errors = [];
 
-    // check required fields
+    // check required fields have an entry
     if(!signUpEmail || !signUpFirstName || !signUpLastName || !signUpUsername || !signUpPassword || !confirmPassword || !signUpCountry) {
         errors.push({msg: "Please fill in all fields"});
     }
@@ -79,27 +79,54 @@ router.post("/users/signup", (req, res) => {
     }
 
     // if there is an error, re-render the page with the errors displayed
-    // else, redirect to the login page
     if(errors.length > 0) {
         res.render("sign-up", {
             errors
         })
     } else {
-        const hashedPassword = bcrypt.hashSync(signUpPassword, 10);
-        db.Users.create({
-            email: signUpEmail,
-            username: signUpUsername,
-            password: hashedPassword,
-            firstName: signUpFirstName,
-            lastName: signUpLastName,
-            country: signUpCountry
-        }).then(function (newUser) {
-            res.json(newUser);
+        // check if email already exists in the database
+        db.Users.findOne({
+            where: {
+                email: signUpEmail
+            }
+        }).then((user) => {
+            if(user) {
+                errors.push({msg: "There is already an account with this email address"});
+                res.render("sign-up", {
+                    errors
+                })
+            }
+
+            // check if username already exists in the database
+            db.Users.findOne({
+                where: {
+                    username: signUpUsername
+                }
+            }).then((user) => {
+                if(user) {
+                    errors.push({msg: "This username is already taken, please try another"});
+                    res.render("sign-up", {
+                        errors
+                    })
+                } else {
+                    // all validations successful, create the user & redirect to the login page
+                    const hashedPassword = bcrypt.hashSync(signUpPassword, 10);
+                    db.Users.create({
+                        email: signUpEmail,
+                        username: signUpUsername,
+                        password: hashedPassword,
+                        firstName: signUpFirstName,
+                        lastName: signUpLastName,
+                        country: signUpCountry
+                    }).then(function () {
+                        res.redirect("/login");
+                    })
+                }
+            })
         })
-        res.redirect("/login");
     }
 
-    console.log(errors);
+    // console.log(errors);
 
 });
 
