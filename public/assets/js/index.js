9eabac5607dcd6104654288cad18fc39669c2ea8
chstrcpprpt <cjhallen@gmail.com>
2020-01-31 06:04:11 +1100

    removed login button click event as this is handled through the form

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -7,31 +7,6 @@ $(function () {
         //need to add some logic
     });
 
-    // $("#login-button-proceed").on("click", function () {        
-    //     //need to add some logic
-    //     event.preventDefault();
-    //     console.log("clicked");
-    //     const usernameInput = $("#login-username").val().trim();
-    //     const passwordInput = $("#login-password").val().trim();
-        
-    //     const checkUserExist = {
-    //         username: usernameInput,
-    //         password: passwordInput,
-    //     };
-    //     // Send the PUT request.
-    //     $.ajax("/api/users/login", {
-    //         type: "POST",
-    //         data: checkUserExist
-    //     }).then(
-    //         function (data) {
-    //             console.log("user has logged in: " + data);                
-    //             // Reload the page to get the updated list
-    //             // location.reload();
-    //         }
-    //     );   
-
-    // });
-
     $("#sign-up-button").on("click", function () {
         //need to add some logic
         console.log("clicked");
