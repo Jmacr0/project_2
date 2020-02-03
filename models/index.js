5124d5d068a1c75f760fe78aafe81d4d9a1c0a1c
chstrcpprpt <cjhallen@gmail.com>
2020-02-03 18:47:10 +1100

    update connection

--- models/index.js
+++ models/index.js
@@ -5,12 +5,17 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.local.json')[env];
+
+let config;
+if (env === 'development') {
+  config = require(__dirname + '/../config/config.local.json')[env];
+}
+
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (!config) {
+  sequelize = new Sequelize(process.env['JAWSDB_URL'], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
